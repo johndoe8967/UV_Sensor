41287525ae04e6b7a58563c1b8642ad8db828844
John Doe <johndoe8967@gmx.at>
2016-06-19 16:33:15 +0200

    credentials in dedicated file

---
+++ include/credentials.h
@@ -0,0 +1,19 @@
+/*
+ * credentials.h
+ *
+ *  Created on: 19.06.2016
+ *      Author: johndoe
+ */
+
+#ifndef APP_CREDENTIALS_H_
+#define APP_CREDENTIALS_H_
+
+
+// If you want, you can define WiFi settings globally in Eclipse Environment Variables
+#define WIFISSID "PleaseEnterSSID" // Put you SSID and Password here
+#define WIFIPWD "PleaseEnterPass"
+#define APIKEY "--";
+
+
+
+#endif /* APP_CREDENTIALS_H_ */
